1f4e55d638e65bf0e5e51eaf79260ed1f7f73ada
trueotmor <proskurin.sasha78@gmail.com>
2023-03-27 12:53:48 +0300

    adad

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const webAppUrl  = 'https://sage-zabaione-378b67.netlify.app';
 const bot = new TelegramBot(process.env.ACCSHOP_TOKEN, {polling: true});
 
-const client = new MongoClient('mongodb+srv://goodorfood:fuHJ53mSvrpwXyD@accshop.11sxcrx.mongodb.net/?retryWrites=true&w=majority')
+const client = new MongoClient(DB_URL)
 
 const start = async () => {
   try {
